b1a4198ffb86cdc4ae04fbbeeebed8f7a3e33f0b
Roberts Bralis <roberts.bralis@testdevlab.com>
2018-01-02 14:38:14 +0200

    Create collection/create request

--- features/endpoint_functions/project_func.rb
+++ features/endpoint_functions/project_func.rb
@@ -46,3 +46,71 @@ def create_collection(name)
   assert_equal(name.to_s,responce_hash['name'],"Creating collection failed! Responce: #{responce}")
   @collections.push(responce_hash)
 end
+
+def create_login_request
+  request_name='login_case'
+  request_payload={:method=>'POST',
+                   :url=>'https://www.apimation.com/login',
+                   :type=>'raw',
+                   :body=>{:login=>@test_user.email,:password=>@test_user.password}.to_json,
+                   :binaryContent=>{:value=>'',:filename=>''}.to_json,
+                   :urlEncParams=>{:name=>'',:value=>''}.to_json,
+                   :formData=>{:type=>'text',:value=>'',:name=>'',:name=>'',:filename=>''}.to_json,
+                   :headers=>{:name=>'Content-Type',:value=> 'application/json'}.to_json,
+                   :greps=>[],
+                   :auth=>{:type=>'noAuth',:data=>{}}.to_json}
+
+  steps_payload={:collection_id=>@collections.last['id'],
+                 :description=>'',
+                 :name=>request_name,
+                 :paste=>false,
+                 :request=>request_payload}
+
+  responce=post('https://www.apimation.com/steps',
+                headers: {'Content-Type'=>'application/json'},
+                cookies: @test_user.session_cookie,
+                payload: steps_payload)
+  #Check if 200 OK is received
+  assert_equal(200, responce.code,"Creating request failed! Responce: #{responce}")
+  responce_hash=JSON.parse(responce)
+  # Check if correct collection
+  assert_equal(@collections.last['id'],responce_hash['collection_id'],"Incorrect collection  returned! Responce: #{responce}")
+  # Check if correct request name
+  assert_equal(request_name,responce_hash['name'],"Incorrect request name returned! Responce: #{responce}")
+  #Check if correct url is returned
+  assert_equal(@collections.last['url'],'https://www.apimation.com/login',"Incorrect URL returned! Responce: #{responce}")
+end
+
+def create_project_request
+  request_name='set_active_project'
+  request_payload={:method=>'PUT',
+                   :url=>'https://www.apimation.com/projects/active/'+@project.project_id,
+                   :type=>'raw',
+                   :binaryContent=>{:value=>'',:filename=>''}.to_json,
+                   :urlEncParams=>{:name=>'',:value=>''}.to_json,
+                   :formData=>{:type=>'text',:value=>'',:name=>'',:name=>'',:filename=>''}.to_json,
+                   :headers=>{:name=>'Content-Type',:value=> 'application/json'}.to_json,
+                   :greps=>[],
+                   :auth=>{:type=>'noAuth',:data=>{}}.to_json}
+
+  steps_payload={:collection_id=>@collections.last['id'],
+                 :description=>'',
+                 :name=>request_name,
+                 :paste=>false,
+                 :request=>request_payload}
+
+  responce=post('https://www.apimation.com/steps',
+                headers: {'Content-Type'=>'application/json'},
+                cookies: @test_user.session_cookie,
+                payload: steps_payload)
+  #Check if 200 OK is received
+  assert_equal(200, responce.code,"Creating request failed! Responce: #{responce}")
+  responce_hash=JSON.parse(responce)
+  # Check if correct collection
+  assert_equal(@collections.last['id'],responce_hash['collection_id'],"Incorrect collection  returned! Responce: #{responce}")
+  # Check if correct request name
+  assert_equal(request_name,responce_hash['name'],"Incorrect request name returned! Responce: #{responce}")
+  #Check if correct url is returned
+  assert_equal(@collections.last['url'],'https://www.apimation.com/login',"Incorrect URL returned! Responce: #{responce}")
+
+end
