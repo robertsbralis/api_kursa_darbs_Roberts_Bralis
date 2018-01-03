0d558c4d0e3ba66d9d9b95e87296fd5caf0d78df
Roberts Bralis <roberts.bralis@testdevlab.com>
2018-01-03 13:23:51 +0200

    Added case creation case

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,8 @@ Before() do
   @test_user = User.new('bralisroberts@gmail.com','parole')
   @project=Project.new('4ffea760-d67b-11e7-8bcd-5d3e2d5d7554')
   @collections ||=[]
+  @requests ||=[]
+  @test_cases ||=[]
 end
 After() do
   @project.environments.each do |env|
@@ -9,5 +11,9 @@ After() do
   end
   @collections.reverse.each do |col|
       delete_collection(col['name'])
-    end
+  end
+
+  @test_cases.each do |test|
+    delete_test_case(test['name'])
+  end
 end
